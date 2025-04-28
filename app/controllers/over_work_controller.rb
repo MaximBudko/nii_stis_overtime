0ac5d4456fc5829fc6cc3becc2712a0161143527
Maxim Budko <maksimbudko16@gmail.com>
2025-04-28 10:14:58 +0000

    revork some one

--- app/controllers/over_work_controller.rb
+++ app/controllers/over_work_controller.rb
@@ -1,3 +1,6 @@
+require 'docx'
+require 'tempfile'
+
 class OverWorkController < ApplicationController
   def generate_overtime_report
 
@@ -61,13 +64,93 @@ class OverWorkController < ApplicationController
     end
   end
 
-  def generate_ov_doc_rep
-    user_ids = params[:user_ids] || []
-    @global_users_word = user_ids
+  # def generate_ov_doc_rep
+  #   user_ids = params[:user_ids] || []
+  #   @global_user_ids = user_ids
+  #   start_date_str = params[:start_date]
+  #   end_date_str = params[:end_date]
+  #   type_overwork = params[:option_select]
+  
+  #   @global_type_overwork = type_overwork.to_s
+  #   output_users = []
+  #   users_loop = User.find(@global_user_ids)
+  #   output_group = ""
+  #   users_loop.each do |user|
+  #     job_group = user.groups
+  #     job_group.each do |group|
+  #       if group.name == "Испытательная лаборатория"
+  #         output_group = "испытательной лаборатории"
+  #       elsif group.name == "Конструкторско-технологический отдел"
+  #         output_group = "конструкторско-технологического отдела"
+  #       elsif group.name == "Сектор встроенного программного обеспечения"
+  #         output_group = "сектора встроенного программного обеспечения"
+  #       elsif group.name == "Сектор источников питания"
+  #         output_group = "сектора источников питания"
+  #       elsif group.name == "Сектор конструирования РЭА"
+  #         output_group = "сектора конструирования РЭА"
+  #       elsif group.name == "Сектор печатных плат"
+  #         output_group = "сектора печатных плат"
+  #       elsif group.name == "Тематический отдел №1"
+  #         output_group = "тематического отдела №1"
+  #       elsif group.name == "Тематический отдел №2"
+  #         output_group = "тематического отдела №2"
+  #       elsif group.name == "Управление по научно-технической работе"
+  #         output_group = "управления по научно-технической работе"
+  #       end
+  
+  #       custom_field_users_surname = user.custom_field_values.detect { |cf| cf.custom_field.name == 'surname' }
+  #       custom_field_users_job_title = user.custom_field_values.detect { |cf| cf.custom_field.name == 'job_title' }
+  
+  #       output_users << "#{custom_field_users_job_title} #{output_group} #{user.firstname.to_s[0]}.#{custom_field_users_surname.to_s[0]}. #{user.lastname} (Работы по ...)"
+  #     end
+  #   end
+  
+  #   #users_text = output_users.join("\n")
+  
+  #   # Выбор шаблона в зависимости от типа переработки
+  #   template_filename = type_overwork == "option_1" ? "temp_wknd.docx" : "temp_ov.docx"
+  
+  #   # Новый путь до шаблона — с Рабочего стола
+  #   template_path = File.expand_path("~/Desktop/template/#{template_filename}")
+  
+  #   unless File.exist?(template_path)
+  #     render plain: "Шаблон не найден: #{template_path}", status: 404
+  #     return
+  #   end
+  
+  #   doc = Docx::Document.open(template_path)
 
-  end
-
-
+  #   doc.paragraphs.each do |p|
+  #     p.each_text_run do |r|
+  #       if r.text.include?('{{user_and_users_data}}')
+  #         r.text = '' # чистим место
+  #       end
+  #     end
+  #   end
+    
+  #   # Потом уже добавляем новые параграфы
+  #   output_users.each_with_index do |user_text, index|
+  #     new_paragraph = doc.paragraphs.first.insert_paragraph_after("#{index + 1}. #{user_text}")
+  #   end
+        
+  
+  #   tmp_file = Tempfile.new(['generated_', '.docx'])
+  #   doc.save(tmp_file.path)
+  #   tmp_file.close
+  
+  #   send_file tmp_file.path,
+  #             filename: "Служебная_записка_#{Time.now.strftime('%d.%m.%Y')}.docx",
+  #             type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  #             disposition: 'attachment',
+  #             stream: false
+  
+  #   # Удаление временного файла
+  #   Thread.new do
+  #     sleep 5
+  #     tmp_file.unlink if File.exist?(tmp_file.path)
+  #   end
+  # end
+  
   private
 
   def load_form_data
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RedmineApp::Application.routes.draw do
     get '/overtime_report', to: 'over_work#generate_overtime_report', as: 'generate_overtime_report'
     post '/overtime_generate', to: 'over_work#do_generate_ov', as: 'do_generate_ov'
+    post '/memo_generate', to: 'over_work#generate_ov_doc_rep', as: 'generate_ov_doc_rep'
 end
